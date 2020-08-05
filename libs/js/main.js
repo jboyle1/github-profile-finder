8ee7586ca8df56139a7bc808e79fe73fa63ef4ed
Jed Boyle <jedboyle01@gmail.com>
2020-08-05 12:19:48 +0100

    Create ajax object including API url that concatenates the typed user name to the end. Also add clien_id and client_secret codes for more data access. Add Ajax success function that runs if the status is OK and then targets the #profile ID then updates the HTML with the typed name.

--- libs/js/main.js
+++ libs/js/main.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
     $('#searchUser').on('keyup', function(e) {
         let username = e.target.value;
 
-        
-    })
-})
+        // Make request to GitHub
+        $.ajax({
+            url: 'https://api.github.com/users/' + username,
+            data: {
+                client_id: '50dc0c99af8152312e7b',
+                client_secret: '9688c037ddc2b2f7a8851b2736c6ca9b95fdc942'
+            },
+            success: function(result) {
+                $('#profile').html(`
+                    ${result.name}
+                `)
+            }
+        });
+    });
+});
