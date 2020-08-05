1d247f2df3421b7d31668aae3662599299a47b28
Jed Boyle <jedboyle01@gmail.com>
2020-08-05 12:38:16 +0100

    Use template literals to insert data from the github api. Use Bootstrap to style the profile results page.

--- libs/js/main.js
+++ libs/js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('#searchUser').on('keyup', function(e) {
+$(document).ready(function () {
+    $('#searchUser').on('keyup', function (e) {
         let username = e.target.value;
 
         // Make request to GitHub
@@ -9,10 +9,36 @@ $(document).ready(function() {
                 client_id: '50dc0c99af8152312e7b',
                 client_secret: '9688c037ddc2b2f7a8851b2736c6ca9b95fdc942'
             },
-            success: function(result) {
-                $('#profile').html(`
-                    ${result.name}
-                `)
+            success: function (user) {
+                $('#profile').html(
+                    `<div class="card border-primary mb-3" style="max-width: 100rem;">
+                    <div class="card-header"><h3>${user.name}</h3></div>
+                    <div class="card-body">
+                      <div class="row">
+                      <div class="col-md-3">
+                        <img class="img-thumbnail avatar" src="${user.avatar_url}">
+                        <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+                      </div>
+                      <div class="col-md-9">
+                        <span class="badge badge-dark">Public Repos: ${user.public_repos}</span>
+                        <span class="badge badge-primary">Public Gists: ${user.public_gists}</span>
+                        <span class="badge badge-success">Followers: ${user.followers}</span>
+                        <span class="badge badge-info">Following: ${user.following}</span>
+                        <br><br>
+                        <ul class="list-group">
+                          <li class="list-group-item">Company: ${user.company}</li>
+                          <li class="list-group-item">Website/blog: <a href="${user.blog}" target="_blank">${user.blog}</a></li>
+                          <li class="list-group-item">Location: ${user.location}</li>
+                          <li class="list-group-item">Member Since: ${user.created_at}</li>
+                        </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <h3 class="page-header">Latest Repos</h3>
+                  <div id="repos"></div>
+                `
+                )
             }
         });
     });
